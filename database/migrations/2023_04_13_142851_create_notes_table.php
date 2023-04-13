b0de5212e3cb810689515dce220a87159e08946f
Jean Jonathan <jeanjonathank2@gmail.com>
2023-04-13 16:38:05 +0200

    Implementation des migrations pour la table notes et videos

--- database/migrations/2023_04_13_142851_create_notes_table.php
+++ database/migrations/2023_04_13_142851_create_notes_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
+            $table->string('chapter');
+            $table->string('level');
+            $table->text('note');
             $table->timestamps();
         });
     }
