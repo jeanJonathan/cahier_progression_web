bd6a2df577a79bb50103acdfa5bd9eaa33348b59
Jean Jonathan <jeanjonathank2@gmail.com>
2023-04-13 16:39:28 +0200

    Implementation de la migration pour la table videos

--- database/migrations/2023_04_13_143734_create_videos_table.php
+++ database/migrations/2023_04_13_143734_create_videos_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('videos', function (Blueprint $table) {
             $table->id();
+            $table->string('sport');
+            $table->string('level');
+            $table->string('url');
             $table->timestamps();
         });
     }
