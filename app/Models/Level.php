c6b695c0f55e6b8d9609a8b3dba031f9c8c6fc70
Jean Jonathan <jeanjonathank2@gmail.com>
2023-04-18 19:29:19 +0200

    suppression de Niveau et creation puis implementation des champs et des relations de la table Level

---
+++ app/Models/Level.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Level extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'nom',
+        'description',
+        'hours_needed',
+        'video_url',
+        'sport_id',
+    ];
+
+    public function sport()
+    {
+        return $this->belongsTo(Sport::class);
+    }
+
+    public function etapes()
+    {
+        return $this->hasMany(Etape::class);
+    }
+
+    public function progressions()
+    {
+        return $this->hasMany(Progression::class);
+    }
+}
