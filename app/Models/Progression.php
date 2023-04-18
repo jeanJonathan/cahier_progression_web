6b4f9afa36e5b373fdf6f07b3ca001306025a006
Jean Jonathan <jeanjonathank2@gmail.com>
2023-04-18 19:21:20 +0200

    Implementation des champs et des relations de la table Progression

--- app/Models/Progression.php
+++ app/Models/Progression.php
@@ -8,4 +8,39 @@ use Illuminate\Database\Eloquent\Model;
 class Progression extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'kite_id',
+        'level_id',
+        'date',
+        'location',
+        'weather',
+        'notes',
+        'photo_url',
+        'video_url',
+        'user_id',
+        'etape_id',
+        'surf_progression',
+        'kite_progression',
+        'wingfoil_progression'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function etape()
+    {
+        return $this->belongsTo(Etape::class);
+    }
+
+    public function kite()
+    {
+        return $this->belongsTo(Kite::class);
+    }
+
+    public function level()
+    {
+        return $this->belongsTo(Level::class);
+    }
 }
