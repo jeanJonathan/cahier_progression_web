3aac817af0656cf88083eeb74ed48b6d90a918c5
Jean Jonathan <jeanjonathank2@gmail.com>
2023-04-18 19:43:17 +0200

    Implementation de la methode index du controller Level

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Level;
 
 class LevelController extends Controller
 {
@@ -13,7 +14,8 @@ class LevelController extends Controller
      */
     public function index()
     {
-        //
+        $levels = Level::all();
+        return view('levels.index', compact('levels'));
     }
 
     /**
