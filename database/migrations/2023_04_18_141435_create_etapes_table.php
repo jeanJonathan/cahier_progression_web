6032968e88e01356bea1e8b258fdf9f9d692fb76
Jean Jonathan <jeanjonathank2@gmail.com>
2023-04-18 16:36:42 +0200

    Implementation des relations entre les differentes tables

---
+++ database/migrations/2023_04_18_141435_create_etapes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('etapes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('progression_id');
+            $table->foreign('progression_id')->references('id')->on('progression');
+            $table->string('nom');
+            $table->text('description')->nullable();
+            $table->string('video_url');
+            $table->unsignedBigInteger('level_id');
+            $table->foreign('level_id')->references('id')->on('levels');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('etapes');
+    }
+};
