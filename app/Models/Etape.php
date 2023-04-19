b49734ec5b7faf34a166fe615a2d9a6beb4e7fa9
Jean Jonathan <jeanjonathank2@gmail.com>
2023-04-19 19:39:16 +0200

    suppresion de la cle etrangere progression_id car la relation y est deja dans la table progression a partir de etape_id

--- app/Models/Etape.php
+++ app/Models/Etape.php
@@ -9,17 +9,11 @@ class Etape extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'progression_id',
         'nom',
         'description',
         'video_url',
         'level_id'
     ];
-
-    public function progression()
-    {
-        return $this->belongsTo('App\Progression');
-    }
 
     public function level()
     {
