351f3a0a833abb9cd2f9a534d526e23d949eb038
Jean Jonathan <jeanjonathank2@gmail.com>
2023-05-10 20:51:24 +0200

    implementation du systeme de verouillage des etapes-1)fonctionnalite de vue etape propre a chaque user.Reccuperation de etape_id envoyer par l'url lors de la selection de l'etape f)systeme de verouillage des etapes non validee par l'utilisateur sauf la derniere etape suivant l'etape validee par user

--- resources/views/etape/index.blade.php
+++ resources/views/etape/index.blade.php
@@ -4,6 +4,10 @@
     <div class="container">
         <h1 style="text-align: center; font-weight: bold; margin-bottom: 20px; font-size: 36px; line-height: 1.2;">{{ $sportNom->name }}</h1>
         <div class="etapes-container">
+            @php
+                $nb_validated_step = 0;
+            @endphp
+
             @foreach ($etapes as $key => $etape)
                 <div class="">
                     <div class="etapes-item-content">
@@ -17,12 +21,23 @@
                                     <img src="{{ asset('isValided.png') }}" alt="Image de l'étape validée {{ $key + 1 }}" class="video" width="220" height="140">
                                     @php
                                         $isValided = true;
+                                        //On increment pour connaitre le nombre d'etape validee
+                                        $nb_validated_step++;
                                     @endphp
                                 @endif
                                 @php
                                     $i++;
                                 @endphp
                             @endwhile
+                            @php
+                            //On initialise nb_step a la valeur totale d'etape contenue dans l'objet etape passe a la vue
+                                $nb_step = count($etapes);
+                                // pour commencer le parcours à partir de la deuxieme étape non validée ainsi la premiere etape suivant l'etape
+                                //validee aura les boutons activee.
+                            $current_step = $nb_validated_step + 1;
+
+                            @endphp
+
                             @if(!$isValided)
                                 <video width="220" height="140" controls style="border-radius: 8px;">
                                     <source src="{{ $etape->video_url }}" type="video/mp4">
@@ -43,14 +58,23 @@
                                     &nbsp; &nbsp; &nbsp;
                                     <a href="{{ $etape->video_url }}" target="_blank" class="btn btn-sm btn-info disabled" style="text-decoration:none;">Voir la vidéo</a>
                                 @else
-                                    <!-- On affiche le bouton valider étape que si l'étape n'est pas validée -->
-                                    <!-- On passe l'identifiant en tant que paramètre dans l'URL de la page suivante -->
-                                    <a href="{{ route('progressions.create', ['etape_id' => $etape->id]) }}" class="btn btn-sm btn-success" style="text-decoration:none;">Valider étape</a>
-                                    &nbsp; &nbsp; &nbsp;
-                                    <!-- On grise le bouton voir vidéo si l'étape n'est pas validée -->
-                                    <a href="{{ $etape->video_url }}" target="_blank" class="btn btn-sm btn-info" style="text-decoration:none;">Voir la vidéo</a>
+                                    @if($loop->iteration >= $current_step)
+                                        <!-- On grise le bouton valider étape et le bouton voir vidéo si l'étape n'a pas encore été atteinte -->
+                                        @if($etape->id == $etapes[$current_step - 1]->id)
+                                            <a href="{{ route('progressions.create', ['etape_id' => $etape->id]) }}" class="btn btn-sm btn-success" style="text-decoration:none;">Valider étape</a>
+                                        @else
+                                            <a href="{{ route('progressions.create', ['etape_id' => $etape->id]) }}" class="btn btn-sm btn-success disabled" style="text-decoration:none;">Valider étape</a>
+                                        @endif
+                                        &nbsp; &nbsp; &nbsp;
+                                        @if($etape->id == $etapes[$current_step - 1]->id)
+                                            <a href="{{ $etape->video_url }}" target="_blank" class="btn btn-sm btn-info" style="text-decoration:none;">Voir la vidéo</a>
+                                        @else
+                                            <a href="{{ $etape->video_url }}" target="_blank" class="btn btn-sm btn-info disabled" style="text-decoration:none;">Voir la vidéo</a>
+                                        @endif
+                                    @endif
                                 @endif
-                        </div>
+
+                            </div>
                         </div>
                     </div>
                 </div>
